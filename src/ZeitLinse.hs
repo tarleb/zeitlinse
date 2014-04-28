a2e0b2b5402112d402caf070a6fa10fe903e3088
Albert Krewinkel <tarleb@moltkeplatz.de>
2014-04-28 10:53:52 +0200

    Update copyright statements to 2014

--- src/ZeitLinse.hs
+++ src/ZeitLinse.hs
@@ -1,21 +1,23 @@
--- ZeitLinse, time dependent rating of information sources
--- Copyright (C) 2013 Albert Krewinkel <tarleb@moltkeplatz.de>
---
--- This file is part of ZeitLinse.
---
--- ZeitLinse is free software: you can redistribute it and/or modify it under
--- the terms of the GNU Affero General Public License as published by the Free
--- Software Foundation, either version 3 of the License, or (at your option)
--- any later version.
---
--- ZeitLinse is distributed in the hope that it will be useful, but WITHOUT
--- ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
--- FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
--- License for more details.
---
--- You should have received a copy of the GNU Affero General Public License
--- along with ZeitLinse.  If not, see <http://www.gnu.org/licenses/>.
+{-
+Copyright (C) 2013–2014 Albert Krewinkel <tarleb@moltkeplatz.de>
 
+This file is part of ZeitLinse.
+
+ZeitLinse is free software: you can redistribute it and/or modify it under
+the terms of the GNU Affero General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option)
+any later version.
+
+ZeitLinse is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
+License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with ZeitLinse.  If not, see <http://www.gnu.org/licenses/>.
+-}
+
+-- | ZeitLinse -- time dependent rating of information sources.
 module ZeitLinse where
 
 import ZeitLinse.Core.Types
--- src/ZeitLinse/Core.hs
+++ src/ZeitLinse/Core.hs
@@ -1,20 +1,21 @@
--- ZeitLinse -- time dependent rating of information sources
--- Copyright (C) 2013 Albert Krewinkel <tarleb@moltkeplatz.de>
+{-
+Copyright (C) 2013–2014 Albert Krewinkel <tarleb@moltkeplatz.de>
 
--- This file is part of ZeitLinse.
+This file is part of ZeitLinse.
 
--- ZeitLinse is free software: you can redistribute it and/or modify it under
--- the terms of the GNU Affero General Public License as published by the Free
--- Software Foundation, either version 3 of the License, or (at your option)
--- any later version.
+ZeitLinse is free software: you can redistribute it and/or modify it under
+the terms of the GNU Affero General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option)
+any later version.
 
--- ZeitLinse is distributed in the hope that it will be useful, but WITHOUT
--- ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
--- FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
--- License for more details.
+ZeitLinse is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
+License for more details.
 
--- You should have received a copy of the GNU Affero General Public License
--- along with ZeitLinse.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU Affero General Public License
+along with ZeitLinse.  If not, see <http://www.gnu.org/licenses/>.
+-}
 
 -- | Iteresting items we which we want to focus on.
 module ZeitLinse.Core
